689d619ca8bb9641e21262a2ff83cd57d79cdc58
khushalGrover <khushal15grover@gmail.com>
2025-11-11 18:17:08 +0530

    vite config js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'r3f-vite-starter',
+  base: 'e-commerce-gallery',
   plugins: [react()],
   server: {
     host: true
