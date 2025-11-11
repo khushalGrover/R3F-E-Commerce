0ff156b096d14a2ce7ef5208d411394f1b41b43e
khushalGrover <khushal15grover@gmail.com>
2025-11-11 16:36:42 +0530

    hello world remove

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Experience } from "./components/Experience";
 function App() {
   return (
     <>
-      hello start
       <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
