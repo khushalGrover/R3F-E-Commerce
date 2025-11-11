f9450eb1541730dc88e32584d598cadd6fec2405
khushalGrover <khushal15grover@gmail.com>
2025-11-11 16:23:57 +0530

    reset Head

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { Canvas } from "@react-three/fiber";
+
 import { Experience } from "./components/Experience";
 
 function App() {
   return (
     <>
-      hello start
-      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+          <Experience />
+     
     </>
-    
   );
 }
 
